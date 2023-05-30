87d1f35449b841bee28836171d1e7fb682021cc7
iamded <dedetmasri@gmail.com>
2023-05-30 10:11:44 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+echo "Dont forget subcribe my chanel: iam ded !!!..."
+sleep 3
+termux-open-url https://youtube.com/channel/UCk0IHP3kOrqN86sXjs1c_Xw
+clear
+sleep 3
+echo "Checking package harap menunggu ya bos..!!!"
+sleep 3
+pkg install python bash git -y
+pip install requests colorama bs4
+pip2 install colorama bs4 requests
+pkg update -y
+pkg upgrade -y
+echo "Package alredy install - running tools"
+clear
+sleep 5
+https://github.com/Hax4us/xerosploit
+cd xerosploit && sudo python install.py
+clear
+sleep 3
+sudo xerosploit
